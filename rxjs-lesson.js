bd5fd9727a63b4129737fdf86aa5f14eb9be47fe
Krister <krister.torsvik@gmail.com>
2017-01-10 09:28:31 +0100

    first lesson with comments

---
+++ rxjs-lesson.js
@@ -0,0 +1,23 @@
+//here we have an observable with an array that maps over each index at 200 milliseconds and it takes 6 items.
+var array = Rx.Observable.interval(200).take(6)
+  .map(i => ['1', '2', '3', '4', 'mate', '6'][i]);
+
+//here we map the array to make it itterate over each index and filter out the strings that is not numbers, then we add them together with reduce
+var FilterAndReduce = array
+    .map( x => parseInt(x))
+    .filter(x => !isNaN(x))
+    .reduce((x, y) => x + y);
+
+//this funtion does nothing until we subscribe to it
+FilterAndReduce.subscribe(x => console.log(x));
+//this function displays nothing until the observable has gone through all the indexes in the array
+//then it returns with the FilterAndReduce value
+//the output will be as follows
+//FilterAndReduce result:            -----------------16->
+//What gets passed to the function:  -'1'--'2'--'3'---'mate'---'6'->
+
+// These two examples would print out to the console every 200 milliseconds:
+//Results from the map would be the sequence that gets parsed printed to the console
+//map output:                        -1--2--3--NaN--6-->
+//Result from only filter would be the sequence of strings that are numbers printed to the console
+// Filter output:                    -1--2--3--4--6-->
